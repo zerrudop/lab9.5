43c06bcca024a28c9358af371745259e7be21e0b
zerrudop <94140890+zerrudop@users.noreply.github.com>
2024-03-22 21:03:35 -0400

    Delete .eleventy.js

--- .eleventy.js
+++
@@ -1,5 +0,0 @@
-// The export statement makes these settings available to other files in 11ty
-module.exports = function(eleventyConfig) {
-  eleventyConfig.addPassthroughCopy("images");
-  eleventyConfig.addPassthroughCopy("styles");
-};
